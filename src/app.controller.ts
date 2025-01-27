22faf1777c98ea0ce21f51671e1b0cf81b3a03e6
paulichdom <paulic.domagoj@gmail.com>
2025-01-27 14:37:33 +0100

    load env, init example controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { openai } from '@ai-sdk/openai';
+import { Controller, Post, Res } from '@nestjs/common';
+import { pipeDataStreamToResponse, streamText } from 'ai';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Post('/data-stream')
+  async root(@Res() res: Response) {
+    const result = streamText({
+      model: openai('gpt-4o'),
+      prompt: 'Invent a new holiday and describe its traditions.',
+    });
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    result.pipeDataStreamToResponse(res);
+  }
+
+  @Post('/stream-data')
+  async streamData(@Res() res: Response) {
+    pipeDataStreamToResponse(res, {
+      execute: async (dataStreamWriter) => {
+        dataStreamWriter.writeData('initialized call');
+
+        const result = streamText({
+          model: openai('gpt-4o'),
+          prompt: 'Invent a new holiday and describe its traditions.',
+        });
+
+        result.mergeIntoDataStream(dataStreamWriter);
+      },
+      onError: (error) => {
+        // Error messages are masked by default for security reasons.
+        // If you want to expose the error message to the client, you can do so here:
+        return error instanceof Error ? error.message : String(error);
+      },
+    });
+  }
+
+  @Post('/text-stream')
+  async example(@Res() res: Response) {
+    const result = streamText({
+      model: openai('gpt-4o'),
+      prompt: 'Invent a new holiday and describe its traditions.',
+    });
+
+    result.pipeTextStreamToResponse(res);
   }
 }
